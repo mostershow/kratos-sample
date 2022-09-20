14bee8a0bff4b20a94f85233a368f21f435fe88b
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-09-20 16:41:18 +0800

    DI && change logger

---
+++ cmd/server/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	logdef "github.com/go-kratos/kratos/contrib/log/logrus/v2"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"os"
+
+	"github.com/go-kratos/kratos-layout/internal/conf"
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	logrus "github.com/sirupsen/logrus"
+)
+
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Name is the name of the compiled software.
+	Name string
+	// Version is the version of the compiled software.
+	Version string
+	// flagconf is the config flag.1
+	flagconf string
+
+	id, _ = os.Hostname()
+)
+
+func init() {
+	flag.StringVar(&flagconf, "conf", "./configs", "config path, eg: -conf config.yaml")
+}
+
+func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
+	return kratos.New(
+		kratos.ID(id),
+		kratos.Name(Name),
+		kratos.Version(Version),
+		kratos.Metadata(map[string]string{}),
+		kratos.Logger(logger),
+		kratos.Server(
+			gs,
+			hs,
+		),
+	)
+}
+
+func main() {
+	flag.Parse()
+
+	logFmt := logrus.New()
+	logFmt.Formatter = &logrus.JSONFormatter{}
+	logFmt.SetLevel(logrus.InfoLevel)
+	logger := logdef.NewLogger(logFmt)
+	logger = log.With(logger,
+		"ts", log.DefaultTimestamp,
+		"caller", log.DefaultCaller,
+		"service.id", id,
+		"service.name", Name,
+		"service.version", Version,
+		"trace.id", tracing.TraceID(),
+		"span.id", tracing.SpanID(),
+	)
+	c := config.New(
+		config.WithSource(
+			file.NewSource(flagconf),
+		),
+	)
+	defer c.Close()
+	logFmt.Info(logrus.Fields{
+		"common": "this is a common filed",
+		"other":  "i also should be logged always",
+	})
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+
+	var bc conf.Bootstrap
+	if err := c.Scan(&bc); err != nil {
+		panic(err)
+	}
+
+	app, cleanup, err := wireApp(bc.Server, bc.Data, logger)
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+
+	// start and wait for stop signal
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
